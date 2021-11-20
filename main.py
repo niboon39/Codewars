613421d7538a1ce5f8db6b2cc16fcfcc1f4bf04a
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2021-11-20 07:11:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,73 @@ def generate_shape(n):
 def nth_even(n):
   # pass
   return (n*2)-2
-test_case = [1 , 2  , 3 , 100 ,1298734] 
-for i in test_case:
-  print("result:",nth_even(i))
+# test_case = [1 , 2  , 3 , 100 ,1298734] 
+# for i in test_case:
+#   print("result:",nth_even(i))
 
+
+# https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/python
+def two_sort(array):
+    # Sort 
+    array = set(array)
+    array = sorted(array, reverse=True)
+    array.sort()
+    # print(array)
+    str1 = array[0]
+    buff = ""
+    for i in range(0,len(str1)):
+      buff += str1[i] 
+      if len(str1)-1 > i:
+       buff+= '***'
+    return buff
+# lst1 = ["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]
+# lst2 = ["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]
+# lst3 = ["i", "want", "to", "travel", "the", "world", "writing", "code", "one", "day"]
+# print(two_sort(lst))
+# l = [lst1 , lst2 , lst3]
+# for i in l : 
+#   print(two_sort(i))
+
+
+# https://www.codewars.com/kata/57cc981a58da9e302a000214/python
+def small_enough(array, limit):
+    buff = []
+    for i in range(len(array)):
+      # print(array[i])
+      if array[i]  <= limit:
+        buff.append(True)  
+      else:
+        buff.append(False)
+    print(buff)
+    if (False in buff):
+      return False 
+    else :
+      return True 
+
+# test =[12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
+# lim = 12
+# print(small_enough(test , lim))
+
+
+# https://www.codewars.com/kata/5511b2f550906349a70004e1/train/python
+def last_digit(n1, n2):
+  if n2 == 0 :return 1 
+  return pow(n1,n2,10)
+
+
+# https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/python
+# 4 kyu
+def solution(string,markers):
+    lst = string.split('\n')
+    for i, s in enumerate(lst):
+        for j in markers:
+            f = s.find(j)
+            s = s[0:f] if f >= 0 else s[:]
+        lst[i] = s.strip()
+
+    return '\n'.join(lst)
+
+
+# string = "apples, pears # and bananas\ngrapes\nbananas !apples"
+# markers = ["#", "!"]
+# print(solution(string , markers))
