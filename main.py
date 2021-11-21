420128b2e329e8f57df27282e7984ec13e9ba589
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2021-11-21 23:28:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def solution(string,markers):
 #   return False 
 
 def is_prime(n):
-  if n == 1: return False 
+  if n <= 1: return False 
   i = 2 
   while i*i <= n :
     if n%i == 0 : return False 
