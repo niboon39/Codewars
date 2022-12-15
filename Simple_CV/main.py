dc1b90e7bf7b1b90173ecc50e3cc007534ced642
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2022-12-16 04:02:03 +1100

    openai

---
+++ Simple_CV/main.py
@@ -0,0 +1,31 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Start the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the output
+    cv2.imshow('Webcam', frame)
+
+    # If the 'q' key is pressed, break the loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the webcam
+cap.release()
